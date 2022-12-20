ed5898dace3c3d1014eef62440306f8bb5f1d415
Millie Cheung <cheungmillie9@gmail.com>
2022-12-20 11:23:31 +0100

    set up firebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
+import { collection, getDocs } from '@firebase/firestore';
 import { Container, Typography } from '@mui/material';
 import { Inter } from '@next/font/google';
 import Head from 'next/head';
-
+import { useCallback } from 'react';
+import { db } from '../firebase';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const postersCollectionRef = collection(db, 'posters');
+
+  const getPosters = useCallback(async () => {
+    const posterData = await getDocs(postersCollectionRef);
+    console.log(
+      posterData.docs.map((doc) => ({ ...(doc.data() as any), id: doc.id }))
+    );
+  }, []);
+
   return (
     <>
       <Head>
