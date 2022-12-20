93bc77d5249b83a1ad10f64f2b30b585a6740876
William Saar <williamsaar@outlook.com>
2022-12-20 10:47:49 +0100

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import { Container, Typography } from '@mui/material';
+import { Inter } from '@next/font/google';
+import Head from 'next/head';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Blue print | Visualize your frames</title>
+        <meta name="description" content="Visualize your frames" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Container>
+          <Typography>Welcome to Blue print</Typography>
+        </Container>
+      </main>
+    </>
+  );
+}
