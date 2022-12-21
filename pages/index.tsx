7a633c41ac085cc39a822d85a86f858d8b5fd962
Millie Cheung <cheungmillie9@gmail.com>
2022-12-21 12:08:01 +0100

    added firebase storage and wrote function for uploading poster (as an object)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { collection, getDocs } from '@firebase/firestore';
-import { Button, Container, Typography } from '@mui/material';
+import { Box, Button, Container, Typography } from '@mui/material';
 import { Inter } from '@next/font/google';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { addDoc, serverTimestamp } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import Head from 'next/head';
-import { useCallback } from 'react';
-import { auth, db } from '../firebase/firebaseConfig';
+import { useCallback, useState } from 'react';
+import { auth, db, storage } from '../firebase/firebaseConfig';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
@@ -23,6 +25,62 @@ export default function Home() {
     await signInWithPopup(auth, provider);
   };
 
+  const [file, setFile] = useState<any>('');
+  const [percent, setPercent] = useState<number>(0);
+
+  // Handles input change event and updates state
+  function handleChange(event: any) {
+    setFile(event.target.files[0]);
+  }
+
+  function handleUpload() {
+    if (!file) alert('Please choose a file first!');
+    const storageRef = ref(storage, `/posters/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const percent = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setPercent(percent);
+      },
+      (err) => console.log(err),
+      () =>
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => addPoster(url))
+    );
+  }
+
+  const addPoster = useCallback(
+    async (url: string) => {
+      const newPoster = {
+        category: 'Animals',
+        createdAt: serverTimestamp(),
+        sizes: [
+          { width: 70, height: 50 },
+          { width: 40, height: 30 },
+          { width: 100, height: 70 },
+        ],
+        title: 'Kapten Krok',
+        orientation: 'Landscape',
+        image: url,
+      };
+      await addDoc(postersCollectionRef, newPoster)
+        .then(() => {
+          setPercent(0);
+          //    navigate('/');
+          //    toast.success('din annons har nu publicerats.', {
+          //      position: toast.POSITION.BOTTOM_CENTER,
+          //    });
+        })
+        .catch((error) => {
+          //    toast.warning(error, { position: toast.POSITION.BOTTOM_CENTER });
+        });
+    },
+    [postersCollectionRef]
+  );
+
   return (
     <>
       <Head>
@@ -35,6 +93,12 @@ export default function Home() {
         <Container>
           <Typography>Welcome to Blue print</Typography>
           <Button onClick={handleGoogleSignIn}>SIGN IN!</Button>
+
+          <Box>
+            <input type="file" accept="image/*" onChange={handleChange} />
+            <Button onClick={handleUpload}>Upload to Firebase</Button>
+            <Typography>{percent}%</Typography>
+          </Box>
         </Container>
       </main>
     </>
