19fe232a14043f17791a399485e34dbcd0d678fa
Millie Cheung <cheungmillie9@gmail.com>
2022-12-22 14:18:11 +0100

    done layout and state logics. The rest to be adjusted when we insert from data

--- components/sidebar/FrameSection.tsx
+++ components/sidebar/FrameSection.tsx
@@ -1,8 +1,19 @@
 import { Box, Checkbox, FormControl, Switch, Typography } from '@mui/material';
+import { IconCheck } from '@tabler/icons';
 import Image from 'next/image';
+import { useState } from 'react';
 import frame from '../../public/tempImages/frame.png';
+import { theme } from '../theme';
 
 const FrameSection = () => {
+  // TODO: add logic - how the states change the frame shown on canvas
+  const [selectedFrame, setSelectedFrame] = useState<String>();
+  const [selectedDimension, setSelectedDimension] = useState<String>('21x30');
+
+  // TODO: get dimensions from data instead
+  const dimensions = ['21x30', '30x40', '40x50', '50x70', '70x100'];
+
+  // To be deleted after we have inserted frames from data
   const showMeFrames = () => {
     const arr = [];
     for (let i = 0; i <= 30; i++) {
@@ -11,10 +22,9 @@ const FrameSection = () => {
     }
     return arr;
   };
-  const dimensions = ['21x30', '30x40', '40x50', '50x70', '70x100'];
 
   return (
-    <Box sx={{ minWidth: 210 }}>
+    <Box sx={{ minWidth: 210, pb: 2 }}>
       <Box
         sx={{
           display: 'flex',
@@ -34,7 +44,7 @@ const FrameSection = () => {
           }}
         >
           <Typography variant="body2">Passepassout</Typography>
-          <Switch />
+          <Switch /> {/* TODO: apply logic */}
         </Box>
       </Box>
       <Box
@@ -47,13 +57,32 @@ const FrameSection = () => {
         }}
       >
         {showMeFrames().map((frame, index) => (
-          <Image
-            key={index}
-            width={40}
-            height={40}
-            alt="frame"
-            src={frame.src}
-          />
+          <Box key={index} sx={{ position: 'relative', height: 40, width: 40 }}>
+            <Image
+              width={40}
+              height={40}
+              alt="frame"
+              src={frame.src}
+              onClick={() => setSelectedFrame(index.toString())}
+            />
+            {/* TODO: adjust logic */}
+            {selectedFrame === index.toString() ? (
+              <IconCheck
+                stroke={1}
+                color={theme.palette.primary.contrastText}
+                size={15}
+                style={{
+                  background: theme.palette.primary.main,
+                  opacity: 0.7,
+                  borderRadius: 50,
+                  padding: 2,
+                  position: 'absolute',
+                  right: 3,
+                  bottom: 3,
+                }}
+              />
+            ) : null}
+          </Box>
         ))}
       </Box>
       <Box
@@ -88,7 +117,13 @@ const FrameSection = () => {
             }}
           >
             <Typography variant="body1">{dimension}</Typography>
-            <Checkbox value={dimension} size="small" sx={{ p: 0 }} />
+            <Checkbox
+              value={dimension}
+              size="small"
+              checked={selectedDimension === dimension}
+              sx={{ p: 0 }}
+              onClick={() => setSelectedDimension(dimension)}
+            />
           </FormControl>
         ))}
       </Box>
