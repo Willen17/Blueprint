53718bdb392ae978555deb30f657bf945efde194
Millie Cheung <cheungmillie9@gmail.com>
2022-12-30 13:26:18 +0100

    adjusted shadows for frames

--- components/shared/CanvasFrame.tsx
+++ components/shared/CanvasFrame.tsx
@@ -36,62 +36,169 @@ const CanvasFrame = (props: Props) => {
   };
 
   const passepartout = () => {
-    let passepartout;
+    let passepartout = 0;
+    // if (withPassepartout) {
     props.size === frameDimensions.xs
-      ? (passepartout = 20)
+      ? (passepartout = 12)
       : props.size === frameDimensions.sm
-      ? (passepartout = 24)
+      ? (passepartout = 16)
       : props.size === frameDimensions.md
-      ? (passepartout = 30)
+      ? (passepartout = 21)
       : props.size === frameDimensions.lg
-      ? (passepartout = 36)
-      : (passepartout = 43);
-    return passepartout;
+      ? (passepartout = 26)
+      : (passepartout = 33);
+    // }
+    return passepartout + frameBorder();
   };
 
   return (
     <Layer draggable>
-      {withPassepartout ? (
-        <>
-          <Rect
-            x={pos().x}
-            y={pos().y}
-            width={props.size.width * 3.5}
-            height={props.size.height * 3.5}
-            fill="#000"
-            shadowBlur={15}
-            shadowColor="black"
-            shadowOpacity={0.5}
-            shadowOffset={{ x: 0, y: 5 }}
-          />
-          <Rect
-            x={pos().x + frameBorder()}
-            y={pos().y + frameBorder()}
-            width={props.size.width * 3.5 - frameBorder() * 2}
-            height={props.size.height * 3.5 - frameBorder() * 2}
-            fill="#f8f8f8"
-          />
-          <Image
-            image={img1}
-            alt="cola"
-            x={pos().x + passepartout()}
-            y={pos().y + passepartout()}
-            width={props.size.width * 3.5 - passepartout() * 2}
-            height={props.size.height * 3.5 - passepartout() * 2}
-            shadowBlur={1}
-            shadowColor="#000"
-            shadowOpacity={0.6}
-            shadowOffset={{ x: 0, y: 0 }}
-          />
-          <Text
-            text={props.size.width + 'x' + props.size.height}
-            x={(pos().x + props.size.width * 3.5) / 2 - 7.5}
-            y={pos().y + props.size.height * 3.5 + 10}
-            fontFamily={theme.typography.fontFamily}
-            fontSize={Number(theme.typography.body1.fontSize)}
-          />
-        </>
-      ) : null}
+      <>
+        <Rect
+          x={pos().x}
+          y={pos().y}
+          width={props.size.width * 3.5}
+          height={props.size.height * 3.5}
+          fill="#fff"
+          shadowBlur={15}
+          shadowColor="#000"
+          shadowOpacity={0.5}
+          shadowOffset={{ x: 0, y: 5 }}
+        />
+
+        <Image
+          image={img1}
+          alt="cola"
+          x={pos().x + frameBorder()}
+          y={pos().y + frameBorder()}
+          width={props.size.width * 3.5 - frameBorder() * 2}
+          height={props.size.height * 3.5 - frameBorder() * 2}
+        />
+        {withPassepartout ? (
+          <>
+            {/*  passepartout. Sequence: left, right, top, bottom*/}
+            <Rect
+              x={pos().x + frameBorder()}
+              y={pos().y + frameBorder()}
+              width={passepartout() - frameBorder()}
+              height={props.size.height * 3.5 - frameBorder() * 2}
+              fill="#f8f8f8"
+            />
+            <Rect
+              x={pos().x + frameBorder()}
+              y={pos().y + frameBorder()}
+              width={props.size.width * 3.5 - frameBorder() * 2}
+              height={passepartout() - frameBorder()}
+              fill="#f8f8f8"
+            />
+            <Rect
+              x={pos().x + props.size.width * 3.5 - passepartout()}
+              y={pos().y + frameBorder()}
+              width={passepartout() - frameBorder()}
+              height={props.size.height * 3.5 - frameBorder() * 2}
+              fill="#f8f8f8"
+            />
+            <Rect
+              x={pos().x + frameBorder()}
+              y={pos().y + props.size.height * 3.5 - passepartout()}
+              width={props.size.width * 3.5 - frameBorder() * 2}
+              height={passepartout() - frameBorder()}
+              fill="#f8f8f8"
+            />
+            {/* shadow for passepartout. Sequence: left, right, top, bottom*/}
+            <Rect
+              x={pos().x + passepartout()}
+              y={pos().y + passepartout() + 1}
+              width={1}
+              height={props.size.height * 3.5 - passepartout() * 2 - 1}
+              fill="#eee"
+              shadowBlur={0.25}
+              shadowColor="#000"
+              shadowOpacity={0.6}
+              shadowOffset={{ x: 0, y: 0 }}
+            />
+            <Rect
+              x={pos().x + props.size.width * 3.5 - passepartout()}
+              y={pos().y + passepartout() + 1}
+              width={1}
+              height={props.size.height * 3.5 - passepartout() * 2 - 1}
+              fill="#eee"
+              shadowBlur={0.25}
+              shadowColor="#000"
+              shadowOpacity={0.6}
+              shadowOffset={{ x: 0, y: 0 }}
+            />
+            <Rect
+              x={pos().x + passepartout()}
+              y={pos().y + passepartout()}
+              width={props.size.width * 3.5 - passepartout() * 2 + 1}
+              height={1}
+              fill="#ddd"
+              shadowBlur={0.5}
+              shadowColor="#000"
+              shadowOpacity={0.2}
+              shadowOffset={{ x: 0, y: 0 }}
+            />
+            <Rect
+              x={pos().x + passepartout()}
+              y={pos().y + props.size.height * 3.5 - passepartout()}
+              width={props.size.width * 3.5 - passepartout() * 2 + 1}
+              height={1}
+              fill="#fff"
+            />
+          </>
+        ) : null}
+        {/* shadow for frame. Sequence: left, right, top, bottom*/}
+        <Rect
+          x={pos().x + frameBorder()}
+          y={pos().y + frameBorder() + 1}
+          width={1}
+          height={props.size.height * 3.5 - frameBorder() * 2 - 1}
+          fill="#eee"
+          shadowBlur={3}
+          shadowColor="#ddd"
+          shadowOpacity={0.7}
+          shadowOffset={{ x: 2, y: 0 }}
+        />
+        <Rect
+          x={pos().x + props.size.width * 3.5 - frameBorder()}
+          y={pos().y + frameBorder() + 1}
+          width={1}
+          height={props.size.height * 3.5 - frameBorder() * 2 - 1}
+          fill="#eee"
+          shadowBlur={3}
+          shadowColor="#ddd"
+          shadowOpacity={0.7}
+          shadowOffset={{ x: -2, y: 0 }}
+        />
+        <Rect
+          x={pos().x + frameBorder()}
+          y={pos().y + frameBorder()}
+          width={props.size.width * 3.5 - frameBorder() * 2 + 1}
+          height={1}
+          fill="#ddd"
+          shadowBlur={13}
+          shadowColor="#000"
+          shadowOpacity={1}
+          shadowOffset={{ x: 0, y: 6 }}
+        />
+        <Rect
+          x={pos().x + frameBorder()}
+          y={pos().y + props.size.height * 3.5 - frameBorder()}
+          width={props.size.width * 3.5 - frameBorder() * 2 + 1}
+          height={1}
+          fill="#fff"
+          opacity={0.5}
+        />
+
+        <Text
+          text={props.size.width + 'x' + props.size.height}
+          x={(pos().x + props.size.width * 3.5) / 2 - 7.5}
+          y={pos().y + props.size.height * 3.5 + 10}
+          fontFamily={theme.typography.fontFamily}
+          fontSize={Number(theme.typography.body1.fontSize)}
+        />
+      </>
     </Layer>
   );
 };
