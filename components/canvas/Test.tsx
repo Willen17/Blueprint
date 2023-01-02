cfce8869befc0db0f81e389c7516a5942986e72d
William Saar <williamsaar@outlook.com>
2023-01-02 10:58:05 +0100

    Made the bacgkround image using konva, instead of CSS

--- components/canvas/Test.tsx
+++ components/canvas/Test.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
-import { Stage } from 'react-konva';
+import { Image, Layer, Stage } from 'react-konva';
+import useImage from 'use-image';
 import { useCanvas } from '../../context/CanvasContext';
 import CanvasFrame from '../shared/CanvasFrame';
 
@@ -33,23 +34,44 @@ function Test() {
     return () => {
       observer.disconnect();
     };
-  }, []);
+  }, [stageCanvasRef]);
+
+  const [canvasBackground] = useImage(background);
+
+  let x, y, width, height;
+  if (canvasBackground) {
+    const aspectRatio = canvasBackground.width / canvasBackground.height;
+
+    const scaleX = dimensions.width / canvasBackground.width;
+    const scaleY = dimensions.height / canvasBackground.height;
+    let scale = scaleX;
+    if (aspectRatio < 1) {
+      scale = Math.max(scaleX, scaleY);
+    } else {
+      scale = Math.min(scaleX, scaleY);
+    }
+    width = canvasBackground.width * scale;
+    height = canvasBackground.height * scale;
+
+    x = (dimensions.width - width) / 2;
+    y = (dimensions.height - height) / 2;
+  }
 
   return (
     <Container sx={{ height: '100%' }} ref={stageCanvasRef}>
-      <Stage
-        height={dimensions.height}
-        width={dimensions.width}
-        style={{
-          backgroundImage: `url(${background})`,
-          maxHeight: dimensions.height,
-          maxWidth: dimensions.width,
-          overflow: 'hidden',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'contain',
-          backgroundPosition: 'center',
-        }}
-      >
+      <Stage height={dimensions.height} width={dimensions.width}>
+        <Layer>
+          {canvasBackground && (
+            <Image
+              image={canvasBackground}
+              alt="Canvas background"
+              x={x}
+              y={y}
+              width={width}
+              height={height}
+            />
+          )}
+        </Layer>
         {frameSet.id && frameSet.size ? (
           <CanvasFrame frameSet={frameSet} />
         ) : null}
